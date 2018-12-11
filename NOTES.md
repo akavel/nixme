e2663e4858308ac39797be9819b21a9c3b2f5d50
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-11 22:10:44 +0100

    initial sketch of ssh-based interceptor

--- NOTES.md
+++ NOTES.md
@@ -13,9 +13,8 @@ appropriate.
    then calls `nix-store --serve` on target machine.
    **We must capture the protocol that ensues, to be able to reproduce it
    later.**
- - To do the above, I want to build a minimal QEMU VM with Nix preinstalled,
-   and with a special SSH service that would have the `nix-store` command
-   overridden to capture stdin & stdout to a file (with `socat`, if easier?).
-   - https://ww.telent.net/2017/10/20/nixos_again_declarative_vms_with_qemu
+ - To do the above, I want to build a  special SSH service that would have the
+   `nix-store` command overridden to capture stdin & stdout to a file (with
+   `socat`, if easier?).
 
 
--- mkvirt.nix
+++ mkvirt.nix
@@ -5,75 +5,40 @@
 , writeText ? pkgs.writeText
 , writeScript ? pkgs.writeScript
 , runCommand ? pkgs.runCommand
+, openssh ? pkgs.openssh
 }:
 
-# Based on/inspired by: https://ww.telent.net/2017/10/20/nixos_again_declarative_vms_with_qemu
+# Based on/inspired by:
+# - https://serverfault.com/questions/344295/is-it-possible-to-run-sshd-as-a-normal-user
+# - https://serverfault.com/questions/407497/how-do-i-configure-sshd-to-permit-a-single-command-without-giving-full-login-ac
+# - http://www.terminalinflection.com/strace-stdin-stdout/
 
 let
-  # result = (iso builtins.currentSystem);
-  result = mkImg;
+  result = startSSH;
 
-  # TODO: build a QEMU image based on iso
-
-  mkImg = writeScript "mkImg" ''
+  startSSH = writeScript "startSSH" ''
     #!${bash}/bin/bash
     set -xeuo pipefail
-    hda="$1"
-    size="$2"
-    [ "$hda" ] && [ "$size" ] || {
-      echo "error: usage: mkImg TARGET_QEMU_DISK.qcow2 SIZE" >&2
-      exit 1
-    }
-    iso="$(echo ${iso builtins.currentSystem}/iso/nixos-*-linux.iso)"
-    ${qemu_kvm}/bin/qemu-img  create -f qcow2  "$hda.tmp" "$size"
-    keydir="$(mktemp -d)"
-    cp "$HOME/.ssh/id_rsa.pub" "$keydir/ssh.pub"
-    ${qemu_kvm}/bin/qemu-kvm \
-      -display vnc=127.0.0.1:99 \
-      -m 512 \
-      -drive file="$hda.tmp",if=virtio \
-      -drive file=fat:floppy:"$keydir",if=virtio,readonly \
-      -drive file=$iso,media=cdrom,readonly \
-      -boot order=d \
-      -serial stdio > "$hda.console.log"
-    if grep INSTALL_SUCCESSFUL "$hda.console.log"; then
-      mv "$hda.tmp" "$hda"
-    fi
+    port="$1"
+
+    ${openssh}/bin/sshd -D -f ${sshdConfig} -p "$port"
   '';
 
+  sshdConfig = writeText "sshd_config" ''
+    UsePrivilegeSeparation no
+    HostKey "${home}/.ssh/id_rsa"
+    PidFile "${home}/var/run/sshd.pid"
+    AuthorizedKeysFile "${home}/.ssh/id_rsa.pub"
+    ForceCommand ${interceptor}
+  '';
 
-  iso = system: (
-    import <nixpkgs/nixos/lib/eval-config.nix> {
-      inherit system;
-      modules = [
-        <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
-        ./modules/nixos-auto-install-service.nix
-      ];
-    }).config.system.build.isoImage;
+  interceptor = writeScript "interceptor" ''
+    #!${bash}/bin/bash
+    mkdir -p ${home}/var/log
+    strace -f -o "${home}/var/log/interceptor.log" -e read,write -e read=0,1,2 -e write=0,1,2 nix-store --serve
+  '';
 
-  #firstRunScript = pkgs.writeScript "firstrun.sh" ''
-  #  #!${bash}/bin/bash
-  #  hda=$1
-  #  size=$2
-  #  iso=$(echo /etc/nixos-cdrom.iso/nixos-*-linux.iso)
-  #  PATH=/run/current-system/sw/bin:$PATH
-  #  ${qemu_kvm}/bin/qemu-img  create -f qcow2  $hda.tmp $size
-  #  mkdir -p /tmp/keys
-  #  cp ${pubkey} /tmp/keys/ssh.pub
-  #  ${qemu_kvm}/bin/qemu-kvm \
-  #    -display vnc=127.0.0.1:99 \
-  #    -m 512 \
-  #    -drive file=$hda.tmp,if=virtio \
-  #    -drive file=fat:floppy:/tmp/keys,if=virtio,readonly \
-  #    -drive file=$iso,media=cdrom,readonly \
-  #    -boot order=d \
-  #    -serial stdio > $hda.console.log
-  #  if grep INSTALL_SUCCESSFUL $hda.console.log; then
-  #    mv $hda.tmp $hda
-  #  fi
-  #'';
-
-  #pubkey = writeText "guest-pubkey" "ssh-rsa AAAATEST=";
+  home = builtins.getEnv "HOME";
 
 in result
 
