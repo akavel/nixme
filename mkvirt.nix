07f37f2eab89b61a3a25c91e0af0631b876e4573
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-11 20:57:48 +0100

    mkImg

--- mkvirt.nix
+++ mkvirt.nix
@@ -3,13 +3,44 @@
 , bash ? pkgs.bash
 , qemu_kvm ? pkgs.qemu_kvm
 , writeText ? pkgs.writeText
+, writeScript ? pkgs.writeScript
+, runCommand ? pkgs.runCommand
 }:
 
 # Based on/inspired by: https://ww.telent.net/2017/10/20/nixos_again_declarative_vms_with_qemu
 
 let
-  # result = stdenv.mkDerivation {};
-  result = (iso builtins.currentSystem);
+  # result = (iso builtins.currentSystem);
+  result = mkImg;
+
+  # TODO: build a QEMU image based on iso
+
+  mkImg = writeScript "mkImg" ''
+    #!${bash}/bin/bash
+    set -xeuo pipefail
+    hda="$1"
+    size="$2"
+    [ "$hda" ] && [ "$size" ] || {
+      echo "error: usage: mkImg TARGET_QEMU_DISK.qcow2 SIZE" >&2
+      exit 1
+    }
+    iso="$(echo ${iso builtins.currentSystem}/iso/nixos-*-linux.iso)"
+    ${qemu_kvm}/bin/qemu-img  create -f qcow2  "$hda.tmp" "$size"
+    keydir="$(mktemp -d)"
+    cp "$HOME/.ssh/id_rsa.pub" "$keydir/"
+    ${qemu_kvm}/bin/qemu-kvm \
+      -display vnc=127.0.0.1:99 \
+      -m 512 \
+      -drive file="$hda.tmp",if=virtio \
+      -drive file=fat:floppy:"$keydir",if=virtio,readonly \
+      -drive file=$iso,media=cdrom,readonly \
+      -boot order=d \
+      -serial stdio > "$hda.console.log"
+    if grep INSTALL_SUCCESSFUL "$hda.console.log"; then
+      mv "$hda.tmp" "$hda"
+    fi
+  '';
+
 
   iso = system: (
     import <nixpkgs/nixos/lib/eval-config.nix> {
