cb9e877c822c94659dd141d5850da360b939c65d
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-12 23:09:48 +0100

    tests: fork mock-io for better write asserts

--- tests/transcripts.rs
+++ tests/transcripts.rs
@@ -2,11 +2,13 @@
 extern crate mock_io;
 use mock_io::Builder;
 use std::io::Read;
+use std::io::Write;
 
 #[test]
 fn existing_pkg_iodump() {
     let mut mock = Builder::open("tests/transcripts/a01-existing-pkg.iodump").unwrap().build();
-    let mut buf = [0; 11];
-    assert_eq!(11, mock.read(&mut buf).unwrap());
-    assert_eq!(&buf[..11], b"hello world");
+    let mut buf = [0; 16];
+    assert_eq!(16, mock.read(&mut buf).unwrap());
+    // assert_eq!(&buf[..11], b"hello world");
+    assert_eq!(11, mock.write(b"hello world").unwrap());
 }
