7652f936578a7b9a5a0f5aacefbff93cc3a89990
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-13 00:18:16 +0100

    lib: starting initial handshake

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
-use std::error::Error;
 use std::io::{Read, Write};
+use byteorder::{ReadBytesExt, WriteBytesExt, LE};
+use failure::{ResultExt, Error, bail};
 
-pub fn serve(mut stream: &(impl Read + Write)) -> Result<(), Box<dyn Error>> {
+// Based on NIX/src/nix-store/nix-store.cc, opServe()
+// Other references:
+// - NIX/src/libstore/legacy-ssh-store.cc
+pub fn serve(stream: &mut (impl Read + Write)) -> Result<(), Error> {
+    // TODO(akavel): add serde helper for reading u32 always as LE, reading strings, etc.
+
+    // Exchange initial greeting.
+    let magic = stream.read_u32::<LE>().context("cannot read 'hello' magic number")?;
+    if magic != SERVE_MAGIC_1 {
+        bail!("protocol mismatch");
+    }
+    stream.write_u32::<LE>(SERVE_MAGIC_2)?;
+    stream.write_u32::<LE>(SERVE_PROTOCOL_VERSION)?;
     Ok(())
 }
+
+const SERVE_MAGIC_1: u32 = 0x390c9deb;
+const SERVE_MAGIC_2: u32 = 0x5452eecb;
+const SERVE_PROTOCOL_VERSION: u32 = 0x205;
+
--- tests/transcripts.rs
+++ tests/transcripts.rs
@@ -7,9 +7,10 @@ use std::io::Write;
 #[test]
 fn existing_pkg_iodump() {
     let mut mock = Builder::open("tests/transcripts/a01-existing-pkg.iodump").unwrap().build();
-    let mut buf = [0; 16];
-    assert_eq!(16, mock.read(&mut buf).unwrap());
-    // assert_eq!(&buf[..11], b"hello world");
-    assert_eq!(11, mock.write(b"hello world").unwrap());
     nixme::serve(&mut mock).unwrap();
+
+    // Mock is empty.
+    let mut buf = [0; 16];
+    assert_eq!(0, mock.read(&mut buf).unwrap());
+    assert!(mock.write(b"X").is_err()); // TODO(akavel): something better should be used here
 }
