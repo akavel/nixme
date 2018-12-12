3f058595121004ad440ba4a631bb4adefa52a5e6
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-12 22:38:41 +0100

    tests: fix read/write direction in iodump files

--- tests/transcripts.rs
+++ tests/transcripts.rs
@@ -1,8 +1,12 @@
 // use nixme;
 extern crate mock_io;
 use mock_io::Builder;
+use std::io::Read;
 
 #[test]
 fn existing_pkg_iodump() {
-    let builder = Builder::open("tests/transcripts/a01-existing-pkg.iodump").unwrap();
+    let mut mock = Builder::open("tests/transcripts/a01-existing-pkg.iodump").unwrap().build();
+    let mut buf = [0; 11];
+    assert_eq!(11, mock.read(&mut buf).unwrap());
+    assert_eq!(&buf[..11], b"hello world");
 }
