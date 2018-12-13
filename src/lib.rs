45d93d103c07f9acb3853311d05575edf49ada39
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-13 17:57:40 +0100

    lib: first attempt at handling EOF

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::io::{Read, Write};
+use std::{io, io::{Read, Write, ErrorKind}};
 use byteorder::{ReadBytesExt, WriteBytesExt, LE};
-use failure::{ResultExt, Error, bail};
+use failure::{ResultExt, Error, bail, Fail};
 
 // Based on NIX/src/nix-store/nix-store.cc, opServe()
 // Other references:
@@ -15,7 +15,26 @@ pub fn serve(stream: &mut (impl Read + Write)) -> Result<(), Error> {
     }
     stream.write_u64::<LE>(SERVE_MAGIC_2)?;
     stream.write_u64::<LE>(SERVE_PROTOCOL_VERSION)?;
-    Ok(())
+    let clientVersion = stream.read_u64::<LE>().context("cannot read client version")?;
+
+    // Handle commands.
+    loop {
+        // TODO(akavel): how to properly handle EOF?
+        let cmd = match stream.read_u64::<LE>() {
+            Ok(x) => x,
+            Err(e) => {
+                // TODO(akavel): make this block less ugly
+                if let Some(cause) = e.cause() {
+                    if let Some(err) = cause.downcast_ref::<io::Error>() {
+                        if err.kind() == ErrorKind::UnexpectedEof {
+                            return Ok(());
+                        }
+                    }
+                }
+                return Err(Error::from(e));
+            },
+        };
+    }
 }
 
 const SERVE_MAGIC_1: u64 = 0x390c9deb;
