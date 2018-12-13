ef2f7e77b780cb707b833970496c4f4f2d16383c
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-13 23:15:45 +0100

    rustfmt

--- tests/transcripts.rs
+++ tests/transcripts.rs
@@ -6,11 +6,17 @@ use std::io::Write;
 
 #[test]
 fn existing_pkg_iodump() {
-    let mut mock = Builder::open("tests/transcripts/a01-existing-pkg.iodump").unwrap().build();
+    let mut mock = Builder::open("tests/transcripts/a01-existing-pkg.iodump")
+        .unwrap()
+        .build();
     nixme::serve(&mut mock).unwrap();
 
     // Mock is empty.
     let mut buf = [0; 16];
-    assert_eq!(0, mock.read(&mut buf).unwrap(), "iodump not yet exhausted for reading");
+    assert_eq!(
+        0,
+        mock.read(&mut buf).unwrap(),
+        "iodump not yet exhausted for reading"
+    );
     assert!(mock.write(b"X").is_err()); // TODO(akavel): something better should be used here
 }
