d32bc3c666b32e050b8be6a0ddc4ccfa7c6c571b
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-13 18:33:09 +0100

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-use std::{io, io::{Read, Write, ErrorKind}};
 use byteorder::{ReadBytesExt, WriteBytesExt, LE};
-use failure::{ResultExt, Error, bail, Fail};
+use failure::{bail, Error, Fail, ResultExt};
+use std::{
+    io,
+    io::{ErrorKind, Read, Write},
+};
 
 // Based on NIX/src/nix-store/nix-store.cc, opServe()
 // Other references:
@@ -9,13 +12,17 @@ pub fn serve(stream: &mut (impl Read + Write)) -> Result<(), Error> {
     // TODO(akavel): add serde helper for reading u64 always as LE, reading strings, etc.
 
     // Exchange initial greeting.
-    let magic = stream.read_u64::<LE>().context("cannot read 'hello' magic number")?;
+    let magic = stream
+        .read_u64::<LE>()
+        .context("cannot read 'hello' magic number")?;
     if magic != SERVE_MAGIC_1 {
         bail!("protocol mismatch");
     }
     stream.write_u64::<LE>(SERVE_MAGIC_2)?;
     stream.write_u64::<LE>(SERVE_PROTOCOL_VERSION)?;
-    let clientVersion = stream.read_u64::<LE>().context("cannot read client version")?;
+    let clientVersion = stream
+        .read_u64::<LE>()
+        .context("cannot read client version")?;
 
     // Handle commands.
     loop {
@@ -32,7 +39,7 @@ pub fn serve(stream: &mut (impl Read + Write)) -> Result<(), Error> {
                     }
                 }
                 return Err(Error::from(e));
-            },
+            }
         };
     }
 }
@@ -41,4 +48,3 @@ const SERVE_MAGIC_1: u64 = 0x390c9deb;
 const SERVE_MAGIC_2: u64 = 0x5452eecb;
 // TODO(akavel): use protocol version 0x205
 const SERVE_PROTOCOL_VERSION: u64 = 0x204;
-
