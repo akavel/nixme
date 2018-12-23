e08d15bed010c7361b77e51689cf657e8edbee97
Mateusz Czapliński <czapkofan@gmail.com>
2018-12-23 20:33:49 +0100

    more attempts at fixing errors

--- src/nar.rs
+++ src/nar.rs
@@ -10,9 +10,13 @@ pub trait Handler {
     fn create_symlink(&mut self, path: &str, target: &str);
 }
 
-pub fn parse(&mut stream: Stream, &mut handler: impl Handler) -> Result<()> {
+pub fn parse<R, H>(stream: &mut Stream<R>, handler: &mut H) -> Result<()>
+where
+    R: Read,
+    H: Handler,
+{
     stream.expect_str(NAR_VERSION_MAGIC_1)?;
-    parse_node(stream, "")
+    parse_node(&mut stream, handler, "")
 }
 
 // FIXME(akavel): why `use crate::stream::protocol_error;` didn't work for me, even with
@@ -25,13 +29,17 @@ macro_rules! protocol_error {
     )
 }
 
-fn parse_node(&mut stream: Stream, &mut handler: impl Handler, path: String) -> Result<()> {
+fn parse_node<R, H>(stream: &mut Stream<R>, handler: &mut H, path: &str) -> Result<()>
+where
+    R: Read,
+    H: Handler,
+{
     stream.expect_str("(")?;
     stream.expect_str("type")?;
-    match stream.read_str_ascii(20).as_ref() {
-        "regular" => parse_file(&mut stream, &mut handler, path),
-        "directory" => parse_directory(&mut stream, &mut handler, path),
-        "symlink" => parse_symlink(&mut stream, &mut handler, path),
+    match stream.read_str_ascii(20)?.as_ref() {
+        "regular" => parse_file(&mut stream, handler, path),
+        "directory" => parse_directory(&mut stream, handler, path),
+        "symlink" => parse_symlink(&mut stream, handler, path),
         other => {
             return protocol_error!(
                 "unexpected node type, should be 'regular'/'directory'/'symlink': '{}'",
@@ -41,7 +49,11 @@ fn parse_node(&mut stream: Stream, &mut handler: impl Handler, path: String) -> 
     }
 }
 
-fn parse_file(&mut stream: Stream, &mut handler: impl Handler, path: String) -> Result<()> {
+fn parse_file<R, H>(stream: &mut Stream<R>, handler: &mut H, path: &str) -> Result<()>
+where
+    R: Read,
+    H: Handler,
+{
     let s = stream.read_str_ascii(20)?;
     let executable = s == "executable";
     let s = if executable {
@@ -52,20 +64,25 @@ fn parse_file(&mut stream: Stream, &mut handler: impl Handler, path: String) -> 
     };
     stream.expect_str("contents")?;
     let (size, &mut blob_stream) = stream.read_blob()?;
-    handler.create_file(path, executable, size, &mut blob_stream)?;
-    stream.expect(")")
+    handler.create_file(path, executable, size, &mut blob_stream);
+    stream.expect_str(")")
 }
 
-fn parse_directory(&mut stream: Stream, &mut handler: impl Handler, path: String) -> Result<()> {
-    handler.create_directory(path)?;
-    let mut prev_name = "";
+fn parse_directory<R, H>(stream: &mut Stream<R>, handler: &mut H, path: &str) -> Result<()>
+where
+    R: Read,
+    H: Handler,
+{
+    handler.create_directory(path);
+    let mut prev_name = "".to_string();
     loop {
-        match stream.read_str_ascii(20).as_ref() {
+        match stream.read_str_ascii(20)?.as_ref() {
             ")" => return Ok(()),
             s if s != "entry" => return protocol_error!("unexpected word in directory: '{}'", s),
+            _ => (),
         }
-        stream.expect("(")?;
-        stream.expect("name")?;
+        stream.expect_str("(")?;
+        stream.expect_str("name")?;
         let name = stream.read_str_ascii(max_name)?;
         if name == "" || name == "." || name == ".." || name.contains("/") || name.contains("\0") {
             return protocol_error!("node name contains invalid characters: '{}'", name);
@@ -74,17 +91,21 @@ fn parse_directory(&mut stream: Stream, &mut handler: impl Handler, path: String
             return protocol_error!("node name not sorted: '{}' <= '{}'", name, prev_name);
         }
         prev_name = name;
-        stream.expect("node")?;
-        parse_node(&mut stream, &mut handler, path + "/" + name)?;
+        stream.expect_str("node")?;
+        parse_node(&mut stream, handler, &(path.to_owned() + "/" + &name))?;
     }
 }
 
-fn parse_symlink(&mut stream: Stream, &mut handler: impl Handler, path: String) -> Result<()> {
-    handler.create_symlink(path, stream.read_str_ascii(max_target)?)?;
-    stream.expect(")")
+fn parse_symlink<R, H>(stream: &mut Stream<R>, handler: &mut H, path: &str) -> Result<()>
+where
+    R: Read,
+    H: Handler,
+{
+    handler.create_symlink(path, &stream.read_str_ascii(max_target)?);
+    stream.expect_str(")")
 }
 
 const max_name: u64 = 255; // FIXME(akavel): use some correct value here; MAX_PATH?
 const max_target: u64 = 255; // FIXME(akavel): use some correct value here; MAX_PATH?
 
-const NAR_VERSION_MAGIC_1: str = "nix-archive-1";
+const NAR_VERSION_MAGIC_1: &str = "nix-archive-1";
