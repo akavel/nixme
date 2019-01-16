0d78878ef0a6c8c5a65542a5981c12e166ad1500
Mateusz Czapliński <czapkofan@gmail.com>
2019-01-16 21:05:37 +0100

    tests/nar-simple: add missed Nix script changes

--- tests/nar-simple/nar-simple.nix
+++ tests/nar-simple/nar-simple.nix
@@ -1,23 +1,33 @@
-with import ./config.nix;
+# based on: github.com/nixos/nix/.../tests/nar-access.*
+{ pkgs ? (import <nixpkgs> {})
+, stdenv ? pkgs.stdenv
+}:
 
 rec {
-    a = mkDerivation {
-        name = "nar-index-a";
-        builder = builtins.toFile "builder.sh"
-      ''
-        mkdir $out
-        mkdir $out/foo
-        touch $out/foo-x
-        touch $out/foo/bar
-        touch $out/foo/baz
-        touch $out/qux
-        mkdir $out/zyx
+  a = stdenv.mkDerivation {
+    name = "nar-simple-a";
+    builder = builtins.toFile "builder.sh" ''
+      source $stdenv/setup
 
-        cat >$out/foo/data <<EOF
-        lasjdöaxnasd
-asdom 12398
-ä"§Æẞ¢«»”alsd
-EOF
-      '';
-    };
+      mkdir $out
+      mkdir $out/foo
+      touch $out/foo-x
+      touch $out/foo/bar
+      touch $out/foo/baz
+      touch $out/qux
+      mkdir $out/zyx
+
+      cat >$out/foo/data <<EOF
+      lasjdöaxnasd
+      asdom 12398
+      ä"§Æẞ¢«»”alsd
+      zażółć gęślą jaźń
+      EOF
+
+      cat >$out/foo/script.sh <<EOF
+      echo hello world
+      EOF
+      chmod a+x $out/foo/script.sh
+    '';
+  };
 }
