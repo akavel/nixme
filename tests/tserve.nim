4a4e0bc2e79e8d786417164d7b5de3c8aa992ace
Mateusz Czapliński <czapkofan@gmail.com>
2019-02-08 21:08:04 +0100

    tests: WIP trying to add 'serve' tests

---
+++ tests/tserve.nim
@@ -0,0 +1,17 @@
+{.experimental: "codeReordering".}
+import unittest
+import transcript
+import nixmepkg/serve
+
+suite "transcripts":
+  test "handshake, then EOF":
+    let session = transcript"""
+-> # 0s  16 bytes
+eb 9d 0c 39 00 00 00 00 04 02 00 00 00 00 00 00   # ...9............ |
+
+<- # 0s  16 bytes
+cb ee 52 54 00 00 00 00 04 02 00 00 00 00 00 00   # ..RT............ |
+"""
+    let store = LocalStore()
+    store.serve(session, session)
+    check session.atEnd
